31ffcf71900b94afc9fd43bb1d881feeddc4c4cb
fishchenko <aleksandr.fishchenko@gmail.com>
2019-08-14 10:38:14 +0300

    move sources to src directory, refs #22007

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "ignore": ['node_modules', './*.js'],
-    "presets": [
-        "@babel/preset-env"
-    ],
-    "plugins": [
-        "@babel/plugin-proposal-class-properties",
-    ],
-};
