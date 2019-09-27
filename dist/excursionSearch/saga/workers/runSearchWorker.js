e2905a2fbce5a455f9e0e46e89df6620662f76e9
fishchenko <aleksandr.fishchenko@gmail.com>
2019-09-27 17:09:12 +0300

    format query excursionSearch request

--- dist/excursionSearch/saga/workers/runSearchWorker.js
+++ dist/excursionSearch/saga/workers/runSearchWorker.js
@@ -7,16 +7,20 @@ exports.runSearchWorker = runSearchWorker;
 
 var _effects = require("redux-saga/effects");
 
+var _moment = _interopRequireDefault(require("moment"));
+
 var _actions = require("../../actions");
 
 var _excursionApi = require("@otpusk/excursion-api");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _marked =
 /*#__PURE__*/
 regeneratorRuntime.mark(runSearchWorker);
 
 function runSearchWorker(_ref) {
-  var queryId, query, tours;
+  var queryId, query, formattedQuery, tours;
   return regeneratorRuntime.wrap(function runSearchWorker$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
@@ -31,32 +35,39 @@ function runSearchWorker(_ref) {
 
         case 4:
           query = _context.sent;
-          _context.next = 7;
-          return (0, _effects.call)(_excursionApi.getSearch, queryId, query.toJS());
+          formattedQuery = query.toMap().map(function (value) {
+            return _moment["default"].isMoment(value) ? value.format('YYYY-MM-DD') : value;
+          }).map(function (value) {
+            return Array.isArray(value) && value.length === 0 ? false : value;
+          }).filter(function (value) {
+            return Boolean(value);
+          }).toJS();
+          _context.next = 8;
+          return (0, _effects.call)(_excursionApi.getSearch, formattedQuery);
 
-        case 7:
+        case 8:
           tours = _context.sent;
-          _context.next = 10;
+          _context.next = 11;
           return (0, _effects.put)(_actions.actions.processSearch(queryId, tours));
 
-        case 10:
-          _context.next = 12;
+        case 11:
+          _context.next = 13;
           return (0, _effects.put)(_actions.actions.finishSearch(queryId));
 
-        case 12:
-          _context.next = 18;
+        case 13:
+          _context.next = 19;
           break;
 
-        case 14:
-          _context.prev = 14;
+        case 15:
+          _context.prev = 15;
           _context.t0 = _context["catch"](1);
-          _context.next = 18;
+          _context.next = 19;
           return (0, _effects.put)(_actions.actions.failSearch(_context.t0));
 
-        case 18:
+        case 19:
         case "end":
           return _context.stop();
       }
     }
-  }, _marked, null, [[1, 14]]);
+  }, _marked, null, [[1, 15]]);
 }
--- src/excursionSearch/saga/workers/runSearchWorker.js
+++ src/excursionSearch/saga/workers/runSearchWorker.js
@@ -1,5 +1,6 @@
 // Core
 import { call, put, select } from 'redux-saga/effects';
+import moment from 'moment';
 
 // Instruments
 import { actions } from '../../actions';
@@ -8,7 +9,12 @@ import { getSearch } from '@otpusk/excursion-api';
 export function* runSearchWorker ({ payload: queryId }) {
     try {
         const query = yield select(({ excursionSearch }) => excursionSearch.getIn(['queries', queryId]));
-        const tours = yield call(getSearch, queryId, query.toJS());
+        const formattedQuery = query.toMap()
+            .map((value) => moment.isMoment(value) ? value.format('YYYY-MM-DD') : value)
+            .map((value) => Array.isArray(value) && value.length === 0 ? false : value)
+            .filter((value) => Boolean(value))
+            .toJS();
+        const tours = yield call(getSearch, formattedQuery);
 
         yield put(actions.processSearch(queryId, tours));
         yield put(actions.finishSearch(queryId));
