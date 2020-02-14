6a326b89fb794c75125c91707475afed25ec2d3a
Oleksander Kozyriev <oleksander.kozyriev@gmail.com>
2020-02-14 16:57:45 +0200

    FormData fixes

--- playground/index.js
+++ playground/index.js
@@ -1,9 +1,19 @@
 import { store } from './store';
-import { excursionSearchActions } from '../src/excursionSearch';
+import { actions } from '../src/excursionAds/actions';
+import { getAgencies } from '@otpusk/excursion-api';
 
 const exec = require('child_process').execSync;
 const sleep = (time) => time && exec(`sleep ${time}`);
 
-store.dispatch(excursionSearchActions.getSearchPrices());
+const query = {
+    cruiseId:   68635,
+    date:       '2020-03-09',
+    operatorId: 464,
+    variantId:  20653084,
+    cityFrom:   1,
+    deptCity:   1397,
+};
+
+store.dispatch(actions.getOffices(query));
 
 sleep(1);
--- src/excursionAds/saga/workers/getAgenciesWorker.js
+++ src/excursionAds/saga/workers/getAgenciesWorker.js
@@ -5,21 +5,19 @@ import { call, put } from 'redux-saga/effects';
 import { actions } from '../../actions';
 import { getAgencies } from '@otpusk/excursion-api';
 
-export function* getAgenciesWorker ({ payload: query }) {
+export function* getAgenciesWorker ({ payload : query }) {
     const { cruiseId: tourId } = query;
-    console.log({ query });
+
     try {
         const formdata = new FormData();
 
-        formdata.append('s', query);
-        console.log({ formdata });
+        formdata.append('s', JSON.stringify(query));
 
-        const { offices, regions } = yield call(getAgencies, formdata);
+        const { regions, offices } = yield call(getAgencies, formdata);
 
         yield put(actions.getRegionsSuccess(regions));
         yield put(actions.getOfficesSuccess(tourId, offices));
     } catch (error) {
-        console.log({ error });
         yield put(actions.getRegionsFail(error));
         yield put(actions.getOfficesFail(tourId));
     }
