3fc19053ede223bf82f119249d40b38c874e8408
Vladyslav Podolskyi <podolskyi.vladyslav@airslate.com>
2021-10-26 13:36:58 +0300

    refactoring offers selectors

--- dist/offers/selectors.js
+++ dist/offers/selectors.js
@@ -5,40 +5,44 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.getOffer = exports.offersHub = void 0;
+exports.getOffer = exports.getOffers = void 0;
 
 var _reselect = require("reselect");
 
 var R = _interopRequireWildcard(require("ramda"));
 
+var _resultsMemory = require("../search/saga/workers/getResultsWorker/resultsMemory");
+
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-// Core
+var getOffersHubFromSearchMemory = function getOffersHubFromSearchMemory(queryID) {
+  return R.prop(queryID, _resultsMemory.memoryInstances) ? _resultsMemory.memoryInstances[queryID].getValues().offersHub : {};
+};
+
 var domain = function domain(_) {
   return _.offers;
 };
 
-var offerKey = function offerKey(_, key) {
-  return key;
-};
+var getOffersStore = (0, _reselect.createSelector)(domain, function (offers) {
+  return offers.get('store');
+});
+var getOffers = (0, _reselect.createSelector)(getOffersStore, function (_) {
+  var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+      queryID = _ref.queryID;
 
-var offersHub = (0, _reselect.createSelector)(domain, R.pipe(function (hub) {
-  return hub.get('store');
-}, R.ifElse(function (v) {
-  return v.isEmpty();
-}, function () {
-  return {};
-}, function (v) {
-  return v.toJS();
-})));
-exports.offersHub = offersHub;
-
-var getOffer = function getOffer() {
-  return (0, _reselect.createSelector)(offersHub, offerKey, function (hub, key) {
-    return R.prop(key, hub);
-  });
-};
-
+  return queryID;
+}, function (offersStore, queryID) {
+  return R.call(R.when(R.always(queryID), function (offers) {
+    return R.mergeAll([offers, getOffersHubFromSearchMemory(queryID)]);
+  }), offersStore.toJS());
+});
+exports.getOffers = getOffers;
+var getOffer = (0, _reselect.createSelector)(getOffers, function (_, _ref2) {
+  var offerID = _ref2.offerID;
+  return offerID;
+}, function (offers, offerID) {
+  return R.prop(offerID, offers);
+});
 exports.getOffer = getOffer;
--- src/offers/selectors.js
+++ src/offers/selectors.js
@@ -1,25 +1,33 @@
-// Core
 import { createSelector } from 'reselect';
 import * as R from 'ramda';
 
-const domain = (_) => _.offers;
-const offerKey = (_, key) => key;
+import { memoryInstances } from '../search/saga/workers/getResultsWorker/resultsMemory';
 
-export const offersHub = createSelector(
+const getOffersHubFromSearchMemory = (queryID) => R.prop(queryID, memoryInstances)
+    ? memoryInstances[queryID].getValues().offersHub
+    : {};
+
+const domain = (_) => _.offers;
+
+const getOffersStore = createSelector(
     domain,
-    R.pipe(
-        (hub) => hub.get('store'),
-        R.ifElse(
-            (v) => v.isEmpty(),
-            () => ({}),
-            (v) => v.toJS()
-        )
+    (offers) => offers.get('store')
+);
+
+export const getOffers = createSelector(
+    getOffersStore,
+    (_, { queryID } = {}) => queryID,
+    (offersStore, queryID) => R.call(
+        R.when(
+            R.always(queryID),
+            (offers) => R.mergeAll([offers, getOffersHubFromSearchMemory(queryID)])
+        ),
+        offersStore.toJS()
     )
 );
 
-
-export const getOffer = () => createSelector(
-    offersHub,
-    offerKey,
-    (hub, key) => R.prop(key, hub)
+export const getOffer = createSelector(
+    getOffers,
+    (_, { offerID }) => offerID,
+    (offers, offerID) => R.prop(offerID, offers)
 );
