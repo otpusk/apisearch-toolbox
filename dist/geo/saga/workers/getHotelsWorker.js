b32775eefdaa8e47d5acf314c9250c468271f52d
Vladyslav Podolskyi <podolskyi.vladyslav@airslate.com>
2021-11-03 20:38:05 +0200

    added lang to hotels method

--- dist/geo/saga/workers/getHotelsWorker.js
+++ dist/geo/saga/workers/getHotelsWorker.js
@@ -14,7 +14,7 @@ var _jsonApi = require("@otpusk/json-api");
 var _marked = /*#__PURE__*/regeneratorRuntime.mark(getHotelsWorker);
 
 function getHotelsWorker(_ref) {
-  var _ref$payload, countryId, withPrice, token, hotels;
+  var _ref$payload, countryId, withPrice, token, lang, hotels;
 
   return regeneratorRuntime.wrap(function getHotelsWorker$(_context) {
     while (1) {
@@ -30,29 +30,37 @@ function getHotelsWorker(_ref) {
         case 4:
           token = _context.sent;
           _context.next = 7;
-          return (0, _effects.call)(_jsonApi.getToursHotels, token, countryId, {
-            withPrice: withPrice
+          return (0, _effects.select)(function (state) {
+            return state.auth.getIn(['otpusk', 'lang'], 'rus');
           });
 
         case 7:
-          hotels = _context.sent;
+          lang = _context.sent;
           _context.next = 10;
-          return (0, _effects.put)(_actions.geoActions.getHotelsSuccess(countryId, hotels));
+          return (0, _effects.call)(_jsonApi.getToursHotels, token, countryId, {
+            lang: lang,
+            withPrice: withPrice
+          });
 
         case 10:
-          _context.next = 16;
+          hotels = _context.sent;
+          _context.next = 13;
+          return (0, _effects.put)(_actions.geoActions.getHotelsSuccess(countryId, hotels));
+
+        case 13:
+          _context.next = 19;
           break;
 
-        case 12:
-          _context.prev = 12;
+        case 15:
+          _context.prev = 15;
           _context.t0 = _context["catch"](1);
-          _context.next = 16;
+          _context.next = 19;
           return (0, _effects.put)(_actions.geoActions.getHotelsFail(_context.t0));
 
-        case 16:
+        case 19:
         case "end":
           return _context.stop();
       }
     }
-  }, _marked, null, [[1, 12]]);
+  }, _marked, null, [[1, 15]]);
 }
--- src/geo/saga/workers/getHotelsWorker.js
+++ src/geo/saga/workers/getHotelsWorker.js
@@ -8,7 +8,9 @@ import { getToursHotels } from '@otpusk/json-api';
 export function* getHotelsWorker ({ payload: { countryId, withPrice }}) {
     try {
         const token = yield select((state) => state.auth.getIn(['otpusk', 'token']));
-        const hotels = yield call(getToursHotels, token, countryId, { withPrice });
+        const lang = yield select((state) => state.auth.getIn(['otpusk', 'lang'], 'rus'));
+
+        const hotels = yield call(getToursHotels, token, countryId, { lang, withPrice });
 
         yield put(geoActions.getHotelsSuccess(countryId, hotels));
     } catch (error) {
