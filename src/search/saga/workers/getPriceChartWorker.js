74a0af2c6867fee9fdf739af6a8a12c90bb6b0e2
Vladyslav Podolskyi <podolskyi.vladyslav@airslate.com>
2022-01-09 12:20:08 +0200

    fixed query params for graph method, refs #37815

--- src/search/saga/workers/getPriceChartWorker.js
+++ src/search/saga/workers/getPriceChartWorker.js
@@ -1,15 +1,12 @@
-// Core
 import { call, put, select } from 'redux-saga/effects';
-import { Range, Set } from 'immutable';
-
-// Instrumetns
 import { getToursGraph } from '@otpusk/json-api';
+
 import { searchActions as actions } from '../../actions';
 import { QUERY_PARAMS } from '../../../queries/fn';
 
 const computedToParam = (query) => {
-    const IS_SET_SINGLE_HOTEL = query.get(QUERY_PARAMS.HOTELS, Set()).size === 1;
-    const IS_SET_SINGLE_CITY = query.get(QUERY_PARAMS.CITIES, Set()).size === 1;
+    const IS_SET_SINGLE_HOTEL = query.get(QUERY_PARAMS.HOTELS).size === 1;
+    const IS_SET_SINGLE_CITY = query.get(QUERY_PARAMS.CITIES).size === 1;
 
     if (IS_SET_SINGLE_HOTEL) {
         return query.get(QUERY_PARAMS.HOTELS).first();
@@ -31,12 +28,13 @@ export function* getPriceChartWorker ({ payload }) {
             token: auth.getIn(['otpusk', 'token']),
         }));
         const params = {
-            to:      computedToParam(query),
-            from:    query.get(QUERY_PARAMS.DEPARTURE),
-            checkIn: query.get(QUERY_PARAMS.DATES).get('from').format('YYYY-MM-DD'),
-            checkTo: query.get(QUERY_PARAMS.DATES).get('from').clone().add(duration || 30, 'days').format('YYYY-MM-DD'),
-            people:  `${query.get(QUERY_PARAMS.ADULTS)}${query.get(QUERY_PARAMS.CHILDREN).map((age) => age < 10 ? `0${age}` : age).join('')}`,
-            nights:  Range(query.get(QUERY_PARAMS.DURATION).get('from') - 1, query.get(QUERY_PARAMS.DURATION).get('to')).toList().join(','),
+            to:       computedToParam(query),
+            from:     query.get(QUERY_PARAMS.DEPARTURE),
+            checkIn:  query.get(QUERY_PARAMS.DATES).get('from').format('YYYY-MM-DD'),
+            checkTo:  query.get(QUERY_PARAMS.DATES).get('from').clone().add(duration || 30, 'days').format('YYYY-MM-DD'),
+            people:   `${query.get(QUERY_PARAMS.ADULTS)}${query.get(QUERY_PARAMS.CHILDREN).map((age) => age < 10 ? `0${age}` : age).join('')}`,
+            nights:   query.get(QUERY_PARAMS.DURATION).get('from'),
+            nightsTo: query.get(QUERY_PARAMS.DURATION).get('to'),
         };
         const chart = yield call(getToursGraph, token, params);
 
