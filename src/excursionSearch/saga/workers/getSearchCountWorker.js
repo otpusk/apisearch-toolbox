5cf094be25299028a448d7189d9ca65fe964d15b
Vladyslav Podolskyi <podolskyi.vladyslav@airslate.com>
2025-09-04 23:29:30 +0200

    added lang to api methods

--- src/excursionSearch/saga/workers/getSearchCountWorker.js
+++ src/excursionSearch/saga/workers/getSearchCountWorker.js
@@ -1,21 +1,25 @@
-// Core
 import { put, call, select } from 'redux-saga/effects';
 import moment from 'moment';
-
-// Instruments
-import { actions } from '../../actions';
 import { getSearchCount } from '@otpusk/excursion-api';
 
-export function* getSearchCountWorker ({ payload: queryId }) {
-    try {
-        const query = yield select(({ excursionSearch }) => excursionSearch.getIn(['queries', queryId]));
-        const formattedQuery = query.toMap()
-            .map((value) => moment.isMoment(value) ? value.format('YYYY-MM-DD') : value)
-            .map((value) => Array.isArray(value) && value.length === 0 ? false : value)
-            .filter((value) => Boolean(value))
-            .toJS();
+import { getLansAsQuery } from '../../../auth/selectors';
+import { actions } from '../../actions';
 
-        const excursionsCount = yield call(getSearchCount, formattedQuery);
+export function* getSearchCountWorker ({ payload: queryId }) {
+    const langAsQuery = yield select(getLansAsQuery);
+    const query = yield select(({ excursionSearch }) => excursionSearch.getIn(['queries', queryId]));
+    const formattedQuery = query.toMap()
+        .map((value) => moment.isMoment(value) ? value.format('YYYY-MM-DD') : value)
+        .map((value) => Array.isArray(value) && value.length === 0 ? false : value)
+        .filter((value) => Boolean(value))
+        .toJS();
+
+    try {
+        const excursionsCount = yield call(getSearchCount, {
+            ...langAsQuery,
+            ...formattedQuery,
+        });
+
         yield put(actions.getExcursionsCountSuccess(queryId, excursionsCount));
     } catch (error) {
         console.log(error);
--- src/excursionSearch/saga/workers/runSearchWorker.js
+++ src/excursionSearch/saga/workers/runSearchWorker.js
@@ -1,22 +1,25 @@
-// Core
 import { call, put, select } from 'redux-saga/effects';
 import moment from 'moment';
-
-// Instruments
-import { actions } from '../../actions';
 import { getSearch } from '@otpusk/excursion-api';
 
-export function* runSearchWorker ({ payload: { queryId, options : { withHash = true } = {}}}) {
-    try {
-        const query = yield select(({ excursionSearch }) => excursionSearch.getIn(['queries', queryId]));
-        const formattedQuery = query.toMap()
-            .map((value) => moment(value, 'YYYY-MM-DD', true).isValid() ? moment(value).format('YYYY-MM-DD') : value)
-            .map((value) => Array.isArray(value) && value.length === 0 ? false : value)
-            .filter((value) => Boolean(value))
-            .toJS();
+import { getLansAsQuery } from '../../../auth/selectors';
+import { actions } from '../../actions';
 
+export function* runSearchWorker ({ payload: { queryId, options : { withHash = true } = {}}}) {
+    const langAsQuery = yield select(getLansAsQuery);
+    const query = yield select(({ excursionSearch }) => excursionSearch.getIn(['queries', queryId]));
+    const formattedQuery = query.toMap()
+        .map((value) => moment(value, 'YYYY-MM-DD', true).isValid() ? moment(value).format('YYYY-MM-DD') : value)
+        .map((value) => Array.isArray(value) && value.length === 0 ? false : value)
+        .filter((value) => Boolean(value))
+        .toJS();
+
+    try {
         const { page } = formattedQuery;
-        const tours = yield call(getSearch, formattedQuery);
+        const tours = yield call(getSearch, {
+            ...langAsQuery,
+            ...formattedQuery,
+        });
 
         if (withHash) {
             const hash = query.compileQuery();
