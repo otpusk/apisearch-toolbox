7d9d8d47d674cef169362fb834847d405ab460f7
jeka1998k <kalinicenko410@gmail.com>
2025-11-11 16:50:43 +0200

    fixed imports

--- src/search/saga/workers/silentSearch.js
+++ src/search/saga/workers/silentSearch.js
@@ -1,15 +1,16 @@
-import { call, select, put } from 'redux-saga/effects';
+import { select, put } from 'redux-saga/effects';
 import * as R from "ramda";
 
-
-import { searchActions } from "../../actions";
 import { getQuery } from "../../../queries/selectors";
-import { QUERY_PARAMS } from "../../../queries/fn";
-import { memoryInstances } from "./getResultsWorker/resultsMemory";
-import { generateNextPrices, getHotelsEntitiesMap, getOffersEntitiesMap } from "./getResultsWorker/helpers";
 import { hotelsHub as getHotelsHub } from "../../../hotels/selectors";
+import { QUERY_PARAMS } from "../../../queries/fn";
 import { hotelsActions } from "../../../hotels";
 import { offersActions } from "../../../offers";
+
+import { searchActions } from "../../actions";
+
+import { memoryInstances } from "./getResultsWorker/resultsMemory";
+import { generateNextPrices, getHotelsEntitiesMap, getOffersEntitiesMap } from "./getResultsWorker/helpers";
 
 
 export function* silentSearch ({ payload : queryID }) {
@@ -18,7 +19,6 @@ export function* silentSearch ({ payload : queryID }) {
     const memory =  memoryInstances[queryID];
 
     const { hotelsHub, offersHub, unusedPrices, stablePrices } = memory.getValues();
-
 
     const nextPrices = generateNextPrices(
         [
@@ -29,7 +29,6 @@ export function* silentSearch ({ payload : queryID }) {
         query.get(QUERY_PARAMS.CURRENCY),
         query.get(QUERY_PARAMS.SELECTED_OPERATORS).toJS()
     );
-
 
     const hotelsFromStore = yield select(getHotelsHub);
     const hotelsToStore = getHotelsEntitiesMap(nextPrices, hotelsHub, hotelsFromStore);
