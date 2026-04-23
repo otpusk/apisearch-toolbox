6acccb24168c5f5ec217b8be3c7a0231252c1c55
Artem Tsys <19successful91@gmail.com>
2026-04-23 10:32:19 +0300

    add using selector

--- src/geo/saga/workers/getGeoTreeWorker.js
+++ src/geo/saga/workers/getGeoTreeWorker.js
@@ -1,12 +1,13 @@
 // Core
 import { put, call, select } from 'redux-saga/effects';
+import { getToken } from '../../../auth/selectors';
 
 // Instruments
 import { geoActions } from '../../actions';
 import { getToursGeoTree } from '@otpusk/json-api';
 
 export function* getGeoTreeWorker ({ payload: { countryId, withPrice, depth = 'city' }}) {
-    const token = yield select((state) => state.auth.getIn(['otpusk', 'token']));
+    const token = yield select(getToken);
 
     const options = {
         ...token,
