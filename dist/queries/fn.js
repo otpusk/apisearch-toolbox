556e7cd6c0845ca549a6335b654ee9392e1f0619
Vladyslav Podolskyi <podolskyi.vladyslav@airslate.com>
2026-06-14 17:25:30 +0200

    released 2.0.40 version

--- dist/queries/fn.js
+++ dist/queries/fn.js
@@ -1,10 +1,9 @@
 "use strict";
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.compileChildrenToPeopleField = exports.QUERY_PARAMS = exports.GLUE = void 0;
+exports.compileChildrenToPeopleField = exports.SHORT_QUERY_NAMES = exports.QUERY_PARAMS = exports.GLUE = void 0;
 exports.compileQuery = compileQuery;
 exports.compileQueryToHash = compileQueryToHash;
 exports.compileSearchQuery = compileSearchQuery;
@@ -18,19 +17,20 @@ var _moment = _interopRequireDefault(require("moment"));
 var _compilers = require("./compilers");
 var _parsers = require("./parsers");
 var _constants = require("./constants");
-var _DEFAULTS;
+var _SHORT_QUERY_NAMES, _DEFAULTS;
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
-function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
-function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
 function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
 function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
-function _arrayWithHoles(r) { if (Array.isArray(r)) return r; } // Core
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); } // Core
 // Instruments
 var CHILD_BIRTHDATE_FORMAT = 'DD.MM.YYYY';
 var CHILD_AGE_FIELD_LENGTH = 2;
@@ -86,27 +86,7 @@ var QUERY_PARAMS = exports.QUERY_PARAMS = {
   IS_DIRECT_FLIGHT: 'isDirectFlight',
   SELECTED_OFFER_DATE: 'offerDate'
 };
-var getShortQueryParams = function getShortQueryParams() {
-  var isParam = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
-  var uniqKeys = new _immutable.Set();
-  var result = {};
-  for (var _i = 0, _Object$entries = Object.entries(QUERY_PARAMS); _i < _Object$entries.length; _i++) {
-    var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
-      key = _Object$entries$_i[0],
-      val = _Object$entries$_i[1];
-    var count = 1;
-    while (count < val.length) {
-      if (!uniqKeys.has(val.slice(0, count))) {
-        break;
-      }
-      count += 1;
-    }
-    uniqKeys = uniqKeys.add(val.slice(0, count));
-    result[isParam ? key : val] = val.slice(0, count);
-  }
-  return result;
-};
-var SHORT_QUERY_NAMES = getShortQueryParams();
+var SHORT_QUERY_NAMES = exports.SHORT_QUERY_NAMES = (_SHORT_QUERY_NAMES = {}, _defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_SHORT_QUERY_NAMES, QUERY_PARAMS.AUTOSTART, 'a'), QUERY_PARAMS.DEPARTURES, 'd'), QUERY_PARAMS.COUNTRY, 'c'), QUERY_PARAMS.CITIES, 'ci'), QUERY_PARAMS.HOTELS, 'h'), QUERY_PARAMS.CATEGORY, 'ca'), QUERY_PARAMS.DATES, 'da'), QUERY_PARAMS.DURATION, 'du'), QUERY_PARAMS.ADULTS, 'ad'), QUERY_PARAMS.CHILDREN, 'ch'), _defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_SHORT_QUERY_NAMES, QUERY_PARAMS.FOOD, 'f'), QUERY_PARAMS.TRANSPORTS, 't'), QUERY_PARAMS.PRICE, 'p'), QUERY_PARAMS.PAGE, 'pa'), QUERY_PARAMS.SERVICES, 's'), QUERY_PARAMS.SHORT, 'sh'), QUERY_PARAMS.RATING, 'r'), QUERY_PARAMS.CURRENCY, 'cu'), QUERY_PARAMS.OPERATORS, 'o'), QUERY_PARAMS.SELECTED_OPERATORS, 'se'), _defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_SHORT_QUERY_NAMES, QUERY_PARAMS.FLIGHT_AVAILABILITY, 'fl'), QUERY_PARAMS.HOTEL_AVAILABILITY, 'ho'), QUERY_PARAMS.WITHOUT_SPO, 'w'), QUERY_PARAMS.LANGUAGE, 'l'), QUERY_PARAMS.NO_AGENCY_STATS, 'n'), QUERY_PARAMS.IGNORE_SERVICES, 'i'), QUERY_PARAMS.GROUP, 'g'), QUERY_PARAMS.DISTRICTS, 'di'), QUERY_PARAMS.PROVINCES, 'pr'), QUERY_PARAMS.AVERAGE_RATING, 'av'), _defineProperty(_defineProperty(_SHORT_QUERY_NAMES, QUERY_PARAMS.IS_DIRECT_FLIGHT, 'is'), QUERY_PARAMS.SELECTED_OFFER_DATE, 'of'));
 
 /**
  * Query defaults
@@ -356,10 +336,10 @@ function convertToOtpQuery(query) {
   }).map(function (value, param) {
     return value ? converters[param](value) : {};
   }).reduce(function (summary, values) {
-    for (var _i2 = 0, _Object$entries2 = Object.entries(values); _i2 < _Object$entries2.length; _i2++) {
-      var _Object$entries2$_i = _slicedToArray(_Object$entries2[_i2], 2),
-        param = _Object$entries2$_i[0],
-        value = _Object$entries2$_i[1];
+    for (var _i = 0, _Object$entries = Object.entries(values); _i < _Object$entries.length; _i++) {
+      var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
+        param = _Object$entries$_i[0],
+        value = _Object$entries$_i[1];
       value && Object.assign(summary, _defineProperty({}, param, param in summary ? summary[param] + value : value));
     }
     return summary;
